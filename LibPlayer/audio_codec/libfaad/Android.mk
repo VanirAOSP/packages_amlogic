48b7332279dd597871048dca02dfca5fdaf257ff
DHOMD <DHO@dho.im>
2013-06-23 21:46:58 -0400

    Fix aliasing
    Yea it's overkill. but took forever to get it

--- LibPlayer/audio_codec/libfaad/Android.mk
+++ LibPlayer/audio_codec/libfaad/Android.mk
@@ -1,12 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+common_flags += -Wno-attributes -fno-strict-aliasing 
+
 include $(CLEAR_VARS)
 LOCAL_MODULE    := libfaad
 LOCAL_SRC_FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
 LOCAL_ARM_MODE := arm
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	  $(LOCAL_PATH)/codebook    
-LOCAL_CFLAGs += -Werror -fno-strict-aliasing 
+LOCAL_CFLAGs += -Wno-attributes -fno-strict-aliasing
 
 include $(BUILD_STATIC_LIBRARY)
 
@@ -21,6 +23,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	 $(LOCAL_PATH)/codebook   
 LOCAL_PRELINK_MODULE := false 
-LOCAL_CFLAGs += -Werror -fno-strict-aliasing 
+LOCAL_CFLAGs += -Wno-attributes -fno-strict-aliasing
 
 include $(BUILD_SHARED_LIBRARY)
